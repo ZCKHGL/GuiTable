c7ca9cdee0462c2d8824ea1af678f16d6ddf5b6f
ZCKHGL <164823089+ZCKHGL@users.noreply.github.com>
2024-12-02 00:26:33 +0700

    Add files via upload

---
+++ TablePert8/src/main/java/com/mycompany/tablepert8/TablePert8.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tablepert8;
+
+/**
+ *
+ * @author excee
+ */
+public class TablePert8 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
